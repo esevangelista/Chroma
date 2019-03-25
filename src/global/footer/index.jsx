9b8fa901158fdc18c7c59c56e554310156f17c3f
esevangelista <esevangelista1@up.edu.ph>
2019-03-26 01:04:55 +0800

    Finish initial UI for homepage

--- src/global/footer/index.jsx
+++ src/global/footer/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import { Route, Switch } from 'react-router-dom';
-import { Layout } from 'antd';
+import { Layout, Row, Col, Divider } from 'antd';
 import './footer.css';
 
 const { Content } = Layout;
@@ -9,7 +9,37 @@ class Footer extends Component {
   render() {
     return (
       <Content>
-        
+        <Row type="flex" justify="center" className="footer-container">       
+          <Col className="col-container" xs={12} sm={6} md={6} lg={6} xl={6} >
+            <h3> Artworks </h3>
+            <a href="/"><p> Form </p></a>
+            <a href="/"><p> Style </p></a>
+            <a href="/"><p> Subject </p></a>
+          </Col>
+          <Col className="col-container" xs={12} sm={6} md={6} lg={6} xl={6} >
+            <h3> Artists</h3>
+            <a href="/"><p> A-Z </p></a>
+            <a href="/"><p> Commission artwork </p></a>
+            <a href="/"><p> Sell on Chroma</p></a>
+          </Col>   
+          <Col className="col-container" xs={12} sm={6} md={6} lg={6} xl={6}>
+            <h3> FAQs</h3>
+            <a href="/"><p> For Artists </p></a>
+            <a href="/"><p> For Art Buyers </p></a>
+          </Col> 
+          <Col className="col-container" xs={12} sm={6} md={6} lg={6} xl={6}>
+            <h3> About </h3>
+            <a href="/"><p> Chroma </p></a>
+            <a href="/"><p> Contact Us </p></a>
+          </Col>
+        </Row>
+        <div className="extra-mask">
+          <div className="extra">
+            <h4>© CHROMA 2019</h4>
+            <a href="/"><p> Terms of Use </p></a>
+            <a href="/"><p> Privacy Policy </p></a>
+          </div>
+        </div>
       </Content>
     );
   }
