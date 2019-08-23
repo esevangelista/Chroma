31aac00dac9ccd8a16ebdaeb34d9441cbfa92785
esevangelista <esevangelista1@up.edu.ph>
2019-08-24 07:06:40 +0800

    Update index

--- src/index.js
+++ src/index.js
@@ -12,19 +12,6 @@ import './index.css';
 
 const APP_ID = '60893392e15857';
 
-CometChat.init(APP_ID).then(
-  () => {
-    /* eslint-disable-next-line no-console */
-    console.log('CometChat: Initialization completed successfully');
-    // You can now call login function.
-  },
-  error => {
-    /* eslint-disable-next-line no-console */
-    console.log('Initialization failed with error:', error);
-    // Check the reason for error and take appropriate action.
-  },
-);
-
 const app = (
   <Provider store={store}>
     <ConnectedRouter history={history}>
@@ -39,3 +26,16 @@ ReactDOM.render(app, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.register();
+
+CometChat.init(APP_ID).then(
+  () => {
+    /* eslint-disable-next-line no-console */
+    console.log('CometChat: Initialization completed successfully');
+    // You can now call login function.
+  },
+  error => {
+    /* eslint-disable-next-line no-console */
+    console.log('Initialization failed with error:', error);
+    // Check the reason for error and take appropriate action.
+  },
+);
